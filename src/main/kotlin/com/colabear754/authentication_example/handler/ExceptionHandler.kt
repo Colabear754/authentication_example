b366abb5631bac01c59eccbc007ffcc037975b34
Colabear754 <jky2406@gmail.com>
2023-05-23 14:37:50 +0900

    예외 핸들링 메소드이름 변경

--- src/main/kotlin/com/colabear754/authentication_example/handler/ExceptionHandler.kt
+++ src/main/kotlin/com/colabear754/authentication_example/handler/ExceptionHandler.kt
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice(basePackages = ["com.colabear754.authentication_example.controller"])
 class ExceptionHandler {
     @ExceptionHandler(IllegalArgumentException::class, NoSuchElementException::class)
-    fun handleIllegalArgumentException(e: Exception) = ResponseEntity.badRequest().body(ApiResponse.error(e.message))
+    fun handleCommonException(e: Exception) = ResponseEntity.badRequest().body(ApiResponse.error(e.message))
 
     @ExceptionHandler(AccessDeniedException::class)
     fun handleAccessDeniedException() = ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiResponse.error("접근이 거부되었습니다."))
 
     @ExceptionHandler(Exception::class)
-    fun handleException() = ResponseEntity.internalServerError().body(ApiResponse.error("서버에 문제가 발생했습니다."))
+    fun handleUnexpectedException() = ResponseEntity.internalServerError().body(ApiResponse.error("서버에 문제가 발생했습니다."))
 }
